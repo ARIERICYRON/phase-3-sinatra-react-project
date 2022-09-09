3c65d5312eb84e546cc2ed337245688ee847d7a0
arieri <ariericyron@gmail.com>
2022-09-09 12:55:49 +0300

    solved

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,24 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/posts" do
+    posts = Posts.all
+    posts.to_json
+  end
+
+  post '/posts' do
+    posts = Posts.create(
+      title: params[:title],
+      author: params[:author],
+      content: params[:content],
+    )
+    posts.to_json
+  end
+
+  delete '/posts/:id' do
+    posts = Posts.find(params[:id])
+    posts.destroy
+    posts.to_json
   end
 
 end
